8d51826621350bffe7724563750938f3e07a8920
Raul <raul.kaskpeit@gmail.com>
2024-12-17 14:17:55 +0200

    IndexDetailsVaade

---
+++ JustShop.Core/Dto/KindergaretnDto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace JustShop2.Core.Dto
+{
+    public class KindergartenDto
+    {
+        public Guid? Id { get; set; }
+        public string GroupName { get; set; }
+        public int? ChildrenCount { get; set; }
+        public string? KindergartenName { get; set; }
+        public string? Teacher { get; set; }
+
+        public List<IFormFile> Files { get; set; }
+        public IEnumerable<FileToDatabaseDto> Image { get; set; }
+            = new List<FileToDatabaseDto>();
+
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ JustShop.Core/Serviceinterface/IKindergartenServices.cs
@@ -0,0 +1,14 @@
+﻿using JustShop2.Core.Domain;
+using JustShop2.Core.Dto;
+
+
+namespace JustShop2.Core.ServiceInterface
+{
+    public interface IKindergartenServices
+    {
+        Task<Kindergarten> DetailAsync(Guid id);
+        //Task<Kindergarten> Update(KindergartenDto dto);
+        //Task<Kindergarten> Delete(Guid id);
+        //Task<Kindergarten> Create(KindergartenDto dto);
+    }
+}
